60328aeafd728dff14f8e8b0fcea23aee3873026
LiamEngelin <liam.engelin@iths.se>
2025-01-22 11:24:23 +0100

    post added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -12,8 +12,20 @@ app.get("/api/messages",(req, res) => {
     res.json(messages);
 });
 
-app.post("/api/",(req, res) => {
-    
+app.post("/api/message",(req, res) => {
+    const { text } = req.body;
+    if(!text){
+        return res.status(400)
+    }
+
+    const newMessage = {
+        id: currentID++,
+        text, 
+        createdAt: new Date()
+    }
+
+    messages.push(newMessage);
+    res.status(200).json({newMessage})
 }); 
 
 app.listen(PORT, (req, res) => {
